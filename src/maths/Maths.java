02319ca250e0d018aa355801cbe13c057b88cd75
koldointxaustiusername <ik013043z1@H013043.NET>
2018-09-25 13:55:17 +0200

    maths

---
+++ src/maths/Maths.java
@@ -0,0 +1,27 @@
+package maths;
+
+public class Maths {
+	public static void main(String args[]) {
+		//addition
+		int sum1 = 10 + 20;
+		int sum2 = sum1 + 10;
+		int sum3 = sum1 + sum2;
+		//substraction
+		int ken1 = 200 - 10;
+		int ken2 = ken1 - 20;
+		int ken3 = ken1 - ken2;
+		//division
+		int div1 = 100 / 10;
+		int div2 = div1 / 2;
+		int div3 = div1 / div2;
+		//multiplication
+		int bid1 = 2 * 2;
+		int bid2 = bid1 * 2;
+		int bid3 = bid1 * bid2;
+		//show the resaults
+		System.out.println("10 + 20= " + sum1);
+		System.out.println("+ 10 =" + sum2);
+		System.out.println("1st resault + 2nd resault =" + sum3);
+		
+	}
+}
